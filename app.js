48009d98572ccb098afcbd55b5a98acb48a65edf
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-04-11 07:01:06 -0400

    app.js mod

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require("express");
+const app = express();
+
+// routes
+app.get("/", (req, res) => {
+  res.send("Task manager App");
+});
+
+const port = process.env.port || 3030;
+
+app.listen(port, console.log(`Server is listening on ${port}...`));
