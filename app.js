2fb0c719d2f273191d70618ba585deea2068ddcd
Wayne Hatter <wayne@yourwpmadesimple.com>
2022-04-11 07:59:19 -0400

    mods

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const app = express();
+const routes = require("./routes/tasks");
 
 // routes
-// app.get("/", (req, res) => {
-//     res.send("Task manager App");
-//   });
+app.get("/", (req, res) => {
+  res.send("Task manager App");
+});
 app.get("/hello", (req, res) => {
   res.send("Task manager App");
 });
+
+// API endpoints
+//app.use("/api/v1/tasks");
 
 const port = process.env.port || 3030;
 
